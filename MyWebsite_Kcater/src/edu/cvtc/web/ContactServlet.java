6db2e57aaa68234f136c2118c6ced276f80d45c9
Katherine Cater <kcater@student.cvtc.edu>
2016-03-23 09:47:13 -0500

    First Commit

---
+++ MyWebsite_Kcater/src/edu/cvtc/web/ContactServlet.java
@@ -0,0 +1,42 @@
+package edu.cvtc.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContactServlet
+ */
+@WebServlet("/ContactServlet")
+public class ContactServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.append("<!DOCTYPE html>\n<html>\n<head>\n<title>My Website</title>\n</head>\n<body>\n");
+		out.append("<header>\n<h1>Contact Form</h1></header>\n\n");
+		out.append("<nav>\n<a href='HomeServlet'>Home</a> <a href='AboutServlet'>About</a>\n</nav>\n");
+		out.append("<div>");
+		out.append("");
+		out.append("</div>\n");
+		out.append("<footer>&copy; Copyright 2016 Katherine Cater</footer>\n");
+		out.append("</body>\n</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
